82f3a3b0c4a81888ab85cac647a9321993421864
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-04-23 18:49:47 +0900

    chore: set up Tailwind CSS
    I prefer Tailwind CSS to react-bootstrap, which is used in this course.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,4 +2,18 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   plugins: ['stylelint-order'],
   ignoreFiles: ['**/node_modules/**'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'layer', 'apply'],
+      },
+    ],
+    'function-no-unknown': [
+      true,
+      {
+        ignoreFunctions: ['theme', 'screen'],
+      },
+    ],
+  },
 };
