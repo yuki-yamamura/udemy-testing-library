d926ff63e4c2f5f23ac269302fd86b661b6ef643
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-04-26 08:07:22 +0900

    fix: fix ESLint error for using label
    see https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/302#issuecomment-425512505

--- .eslintrc.js
+++ .eslintrc.js
@@ -72,6 +72,14 @@ module.exports = {
       },
     ],
     'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
   },
   root: true,
   settings: {
