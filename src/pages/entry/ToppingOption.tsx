8a4bc3026dc804b71ccff16822081879077c92bf
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-04-29 20:59:44 +0900

    feat: code and test subtotal for toppings option

--- src/pages/entry/ToppingOption.tsx
+++ src/pages/entry/ToppingOption.tsx
@@ -1,13 +1,24 @@
 import { Topping } from 'types/Topping';
+import { useOrderDetails } from 'contexts/OrderDetails';
 
 type Props = { topping: Topping };
 
 function ToppingOption({ topping }: Props) {
+  const { updateItemCount } = useOrderDetails();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const toppingCount = e.target.checked ? 1 : 0;
+    updateItemCount(topping.name, toppingCount, 'toppings');
+  };
+
   return (
-    <img
-      src={`http://localhost:3030/${topping.imagePath}`}
-      alt={`${topping.name} topping`}
-    />
+    <div>
+      <label htmlFor={topping.name}>{topping.name}</label>
+      <input id={topping.name} type="checkbox" onChange={handleChange} />
+      <img
+        src={`http://localhost:3030/${topping.imagePath}`}
+        alt={`${topping.name} topping`}
+      />
+    </div>
   );
 }
 
--- src/pages/entry/tests/totalUpdates.test.tsx
+++ src/pages/entry/tests/totalUpdates.test.tsx
@@ -24,3 +24,28 @@ test('update scoop subtotal when scoops change', async () => {
   await user.type(chocolateInput, '2');
   expect(scoopsSubtotal).toHaveTextContent('6.00');
 });
+
+test('update topping subtotal when toppings change', async () => {
+  render(<Options optionType="toppings" />);
+  const user = userEvent.setup();
+
+  const toppingSubtotal = screen.getByText('Toppings total: $', {
+    exact: false,
+  });
+  expect(toppingSubtotal).toHaveTextContent('$0.00');
+
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
+    name: /cherries/i,
+  });
+  await user.click(cherriesCheckbox);
+  expect(toppingSubtotal).toHaveTextContent('$1.50');
+
+  const hotFudgeCheckbox = await screen.findByRole('checkbox', {
+    name: /hot fudge/i,
+  });
+  await user.click(hotFudgeCheckbox);
+  expect(toppingSubtotal).toHaveTextContent('$3.00');
+
+  await user.click(cherriesCheckbox);
+  expect(toppingSubtotal).toHaveTextContent('$1.50');
+});
