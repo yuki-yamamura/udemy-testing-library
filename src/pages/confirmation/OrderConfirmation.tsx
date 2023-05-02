d31ad619a08cb52e6dbbc2ce10eaec1576cbc2fa
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-05-02 14:01:35 +0900

    feat: add error handling logic on order confirmation page

--- src/pages/confirmation/OrderConfirmation.tsx
+++ src/pages/confirmation/OrderConfirmation.tsx
@@ -3,26 +3,35 @@ import { useOrderDetails } from 'contexts/OrderDetails';
 import axios from 'axios';
 import Loading from 'common/Loading';
 import { OrderPhase } from 'types/OrderPhase';
+import AlertBanner from 'common/AlertBanner';
 
 type Props = { setOrderPhase: (orderPhase: OrderPhase) => void };
 
 export default function OrderConfirmation({ setOrderPhase }: Props) {
   const [orderNumber, setOrderNumber] = useState<number | null>(null);
+  const [error, setError] = useState(false);
   const { resetOrders } = useOrderDetails();
   function handleClick() {
     setOrderPhase('inProgress');
     resetOrders();
   }
   useEffect(() => {
+    const controller = new AbortController();
     axios
-      .post('http://localhost:3030/order')
+      .post('http://localhost:3030/order', {
+        signal: controller.signal,
+      })
       .then((res) =>
         setOrderNumber((res.data as { orderNumber: number }).orderNumber),
       )
-      .catch((_) => {
-        // TODO: handle error
+      .catch((err: Error) => {
+        if (err.name !== 'CanceledError') {
+          setError(true);
+        }
       });
   }, []);
+
+  if (error) return <AlertBanner />;
 
   return (
     <div>
---
+++ src/pages/confirmation/test/OrderConfirmation.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from 'test-utils/testing-library-utils';
+import server from 'mocks/server';
+import { rest } from 'msw';
+import OrderConfirmation from '../OrderConfirmation';
+
+test('alert banner appears if server error occurred', async () => {
+  server.resetHandlers(
+    rest.post('http://localhost:3030/order', (req, res, ctx) =>
+      res(ctx.status(500)),
+    ),
+  );
+
+  render(<OrderConfirmation setOrderPhase={jest.fn()} />);
+
+  const alert = await screen.findByRole('alert');
+  expect(alert).toHaveTextContent(
+    'An unexpected error occurred. Please try again later.',
+  );
+});
